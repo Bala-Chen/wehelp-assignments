c386e32bdc6626067188fc3a8521c3c958a30164
bala-chen <84770662+Bala-Chen@users.noreply.github.com>
2022-02-08 12:29:28 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -23,16 +23,18 @@ def signup():
     name = request.form["name"]
     username = request.form["username"]
     password = request.form["password"]
-    cursor.execute('SELECT member.username FROM member;')
+    signup_sql = "SELECT member.username FROM member WHERE username= %s;"
+    signup_val = (username,)
+    cursor.execute(signup_sql,signup_val)
     check_username = cursor.fetchall()
-    if (username,) in check_username :
-        return redirect("/error?message=帳號已經被註冊")
-    elif (username,) not in check_username :
-        sql = "INSERT INTO member(name,username,password) VALUES (%s,%s,%s)"
-        val = (name,username,password)
-        cursor.execute(sql,val)
+    if check_username == []:
+        add_sql = "INSERT INTO member(name,username,password) VALUES (%s,%s,%s)"
+        add_val = (name,username,password)
+        cursor.execute(add_sql,add_val)
         connection.commit()
         return redirect('/')
+    elif check_username != []:
+        return redirect("/error?message=帳號已經被註冊")
     else:
         return redirect("/error?message=預期外的錯誤")
 
@@ -41,13 +43,13 @@ def signin():
     username = request.form["username"]
     password = request.form["password"]
     try:
-        sql = "SELECT * FROM member WHERE username = %s AND password = %s"
-        val = (username,password)
-        cursor.execute(sql,val)
+        signin_sql = "SELECT member.name,username,password FROM member WHERE username = %s AND password = %s"
+        signin_val = (username,password)
+        cursor.execute(signin_sql,signin_val)
         verify_account = cursor.fetchall()
         for data in verify_account:
             main_data = list(data) 
-        session["name"]= main_data[1]
+        session["name"]= main_data[0]
         return redirect("/member")
     except NameError:    
         return redirect("/error?message=帳號或密碼輸入錯誤")
