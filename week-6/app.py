c72cca373541234d93aee9060e4126787b6a9d0c
bala-chen <84770662+Bala-Chen@users.noreply.github.com>
2022-02-21 16:40:37 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -1,18 +1,25 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+from mysql.connector import pooling
 
-connection = mysql.connector.connect(
-    host="localhost",
-    port="3306",
-    user="root",
-    password="test123",
-    database="website"
+load_dotenv()
+
+cnxpool = mysql.connector.pooling.MySQLConnectionPool(
+    pool_name="mypool",
+    pool_size=3,
+    pool_reset_session=True,
+    host=os.getenv("host"),
+    port=os.getenv("port"),
+    user=os.getenv("user"),
+    password=os.getenv("password"),
+    database=os.getenv("database")
 )
-cursor = connection.cursor()
 
 from flask import *
 
 app = Flask(__name__)
-app.secret_key = "test123"
+app.secret_key = os.getenv("secret_key")
 
 @app.route("/")
 def index():
@@ -20,9 +27,13 @@ def index():
 
 @app.route("/signup",methods=["POST"])
 def signup():
+    conn1 = cnxpool.get_connection()
+    cursor = conn1.cursor()
     name = request.form["name"]
     username = request.form["username"]
     password = request.form["password"]
+    if username == '' or password == '':
+        return redirect("/error?message=帳號密碼不得為空")
     signup_sql = "SELECT member.username FROM member WHERE username= %s;"
     signup_val = (username,)
     cursor.execute(signup_sql,signup_val)
@@ -31,7 +42,9 @@ def signup():
         add_sql = "INSERT INTO member(name,username,password) VALUES (%s,%s,%s)"
         add_val = (name,username,password)
         cursor.execute(add_sql,add_val)
-        connection.commit()
+        cursor.close()
+        conn1.commit()
+        conn1.close()
         return redirect('/')
     elif check_username != []:
         return redirect("/error?message=帳號已經被註冊")
@@ -40,6 +53,8 @@ def signup():
 
 @app.route("/signin", methods=["POST"])
 def signin():
+    conn2 = cnxpool.get_connection()
+    cursor = conn2.cursor()
     username = request.form["username"]
     password = request.form["password"]
     try:
@@ -55,6 +70,9 @@ def signin():
         return redirect("/error?message=帳號或密碼輸入錯誤")
     else:
         return redirect("/error?message=預期外的錯誤")
+    finally:
+        cursor.close()
+        conn2.close()
 
 
 @app.route("/member")
